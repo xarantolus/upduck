dfb6a9259f90c94ed0816a2a2b87de6b300c6da0
xarantolus <xarantolus@protonmail.com>
2020-07-29 14:08:54 +0200

    Add optional user logins (closes #4)

--- config.go
+++ config.go
@@ -34,8 +34,13 @@ var (
 	save = flag.Bool("save", false, "Save the given command line arguments to a config file located in your home directory")
 )
 
+const (
+	configFileName = ".upduck.json"
+	userFileName   = ".users.upduck.json"
+)
+
 func usage() {
-	fmt.Fprint(flag.CommandLine.Output(), "upduck, a simple HTTP and HTTPs file server\n\n")
+	fmt.Fprint(flag.CommandLine.Output(), "upduck, a simple HTTP and HTTPS file server\n\n")
 	fmt.Fprintln(flag.CommandLine.Output(), "Command-line flags:")
 	flag.PrintDefaults()
 	fmt.Fprint(flag.CommandLine.Output(),
@@ -53,7 +58,7 @@ Examples:
 
 		upduck -dir path/to/dir
 
-	Start a HTTP server and a HTTPs server:
+	Start a HTTP server and a HTTPS server:
 
 		> upduck -email your@email.com -token DuckDNSToken -site mysite
 
@@ -61,7 +66,7 @@ Examples:
 		This external chosen port you set in the router must be put after the DuckDNS URL, e.g. https://mysite.duckdns.org:525/ for port 525.
 		If you're not sure about how this works, search for "port forward tutorial" and your router model/vendor.
 
-	Start a HTTP server and a HTTPs server on custom ports:
+	Start a HTTP server and a HTTPS server on custom ports:
 
 		> upduck -p 2020 -sp 2121 -email your@email.com -token DuckDNSToken -site mysite
 
@@ -72,12 +77,28 @@ Examples:
 		> upduck -save -p 2020 -email your@email.com -token DuckDNSToken -site mysite
 
 		This will save your current command line. The next time, you will just need to run upduck without arguments to start the same configuration.
-		You can also add the -p flag without impacting your HTTPs configuration.`,
-			"\t", "  "))
+		You can also add the -p flag without impacting your HTTPS configuration.
+
+User configuration:
+	Upduck allows protecting your files by creating user accounts from the command-line.
+
+	Create a new user account (or replace an existing one):
+
+		> upduck adduser <username> <password>
+
+	Delete a user:
+
+		> upduck deluser <username>
+
+	Reset all user logins:
+
+		> upduck resetusers
+
+	If any user accounts are configured, you need to log in before accessing files.`, "\t", "  "))
 }
 
 // ParseConfig parses command-line flags
-func ParseConfig() (c Config, err error) {
+func ParseConfig() (c Config, ustore *UserStore, err error) {
 	flag.Usage = usage
 	flag.Parse()
 
@@ -91,9 +112,82 @@ func ParseConfig() (c Config, err error) {
 		SecurePort:                *securePort,
 	}
 
+	upath := getConfigPath(userFileName)
+
+	// If the user store does not exist, we don't warn about it. It was most likely not set up
+	ustore, err = loadUsers(upath)
+	if err != nil && !os.IsNotExist(err) {
+		log.Println("[Warning] Error while loading user configuration file:", err.Error())
+	} else if err == nil && len(ustore.Users) > 0 {
+		log.Println("Loaded user configuration from", upath)
+	}
+	err = nil // Both errors are not critical and must not be returned
+
+	// Allow creating user profiles
+	//
+	// Examples:
+	// Add user:
+	//     upduck adduser myname mypassword
+	// Remove user:
+	//     upduck deluser myname
+	// Remove all users:
+	//     upduck resetusers
+	if flag.NFlag() == 0 && flag.NArg() > 0 {
+		switch strings.ToLower(flag.Arg(0)) {
+		case "adduser", "useradd", "createuser", "replaceuser":
+			uname, passwd := flag.Arg(1), flag.Arg(2)
+			if uname == "" {
+				log.Fatalln("Username must be given")
+			}
+			if passwd == "" {
+				log.Fatalln("Password must be given")
+			}
+
+			// Add (or replace) that user in the user store
+			ustore.Users[uname] = user{
+				PasswordHash: hash(passwd),
+			}
+
+			err = ustore.Save()
+			if err != nil {
+				log.Fatalln("Error while saving user data:", err.Error())
+			}
+			log.Println("Successfully added user", uname)
+			os.Exit(0)
+		case "deluser", "rmuser", "userdel", "userrm":
+			uname := flag.Arg(1)
+			if uname == "" {
+				log.Fatalln("Username must be given to delete it")
+			}
+
+			// Remove user from the user store
+			delete(ustore.Users, uname)
+
+			err = ustore.Save()
+			if err != nil {
+				log.Fatalln("Error while saving user data:", err.Error())
+			}
+			log.Println("Successfully removed user")
+			os.Exit(0)
+		case "delallusers", "rmallusers", "resetusers":
+			ustore.Users = make(map[string]user)
+
+			err = ustore.Save()
+			if err != nil {
+				log.Fatalln("Error while saving user data:", err.Error())
+			}
+			log.Println("Successfully removed user data")
+			os.Exit(0)
+		}
+	}
+
+	if flag.NArg() != 0 {
+		log.Printf("[Warning] Ignored arguments %q\n", strings.Join(flag.Args(), " "))
+	}
+
 	// If we should save this to the configuration file, we exit afterwards
 	if *save {
-		cfgPath := getConfigPath()
+		cfgPath := getConfigPath(configFileName)
 
 		log.Println("Saving configuration to", cfgPath)
 		log.Println("Please note that from now on any program might be able to get your Email and Token from that file.")
@@ -102,7 +196,7 @@ func ParseConfig() (c Config, err error) {
 
 		f, err := os.Create(cfgPath)
 		if err != nil {
-			return c, err
+			return c, ustore, err
 		}
 		defer f.Close()
 
@@ -111,18 +205,18 @@ func ParseConfig() (c Config, err error) {
 
 		err = enc.Encode(c)
 		if err != nil {
-			return c, err
+			return c, ustore, err
 		}
 
 		log.Println("Successfully saved config file.")
 		os.Exit(0)
 	} else if *duckDNSToken == "" && *duckDNSSite == "" && *letsEncryptEmail == "" {
-		cfgPath := getConfigPath()
+		cfgPath := getConfigPath(configFileName)
 		// If no flags *except* maybe -p have been set, we just load the config file
 		f, err := os.Open(cfgPath)
 		if err != nil {
 			if !os.IsNotExist(err) {
-				return c, err
+				return c, ustore, err
 			}
 			log.Println("[Warn] Config file does not yet exist. You can create one with the -save flag")
 			goto breakout
@@ -131,7 +225,7 @@ func ParseConfig() (c Config, err error) {
 
 		err = json.NewDecoder(f).Decode(&c)
 		if err != nil {
-			return c, err
+			return c, ustore, err
 		}
 
 		log.Println("Loaded config file from", cfgPath)
@@ -171,7 +265,7 @@ breakout:
 }
 
 // getConfigPath returns the config path while respecting certain environment variables
-func getConfigPath() string {
+func getConfigPath(fn string) string {
 	cfgDirPath := os.Getenv("XDG_CONFIG_HOME")
 	if cfgDirPath == "" {
 		hdir, err := os.UserHomeDir()
@@ -188,5 +282,5 @@ func getConfigPath() string {
 		panic("cannot determine user configuration directory")
 	}
 
-	return filepath.Join(cfgDirPath, ".upduck.json")
+	return filepath.Join(cfgDirPath, fn)
 }
