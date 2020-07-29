a04347e5cd51922dd4be5158344c6a7efbe5ec59
xarantolus <xarantolus@protonmail.com>
2020-07-29 11:46:18 +0200

    Build different versions for Raspberry Pis (#3)

--- release.sh
+++ release.sh
@@ -7,4 +7,7 @@ GOOS=windows go build -o "releases/$PROGRAM_NAME-windows.exe"
 
 GOOS=linux go build -o "releases/$PROGRAM_NAME-linux"
 
-GOOS=linux GOARCH=arm GOARM=7 go build -o "releases/$PROGRAM_NAME-raspberrypi"
+GOOS=linux GOARCH=arm GOARM=5 go build -o "releases/$PROGRAM_NAME-raspberrypi-arm5"
+GOOS=linux GOARCH=arm GOARM=6 go build -o "releases/$PROGRAM_NAME-raspberrypi-arm6"
+GOOS=linux GOARCH=arm GOARM=7 go build -o "releases/$PROGRAM_NAME-raspberrypi-arm7"
+
